9ca69b9cf3bd5215bab6f8304d493b7cf7202103
Harshath <harshathmkulal@gmail.com>
2024-09-27 09:18:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "9fce2f59cc7444cbbceafff66907dcd8";
+const API_KEY = "API_KEY";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
